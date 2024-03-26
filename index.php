9ae06c4906d2eccea0e668ec003dd3670e6b0af0
razabangi <muhammadrazabangi9@gmail.com>
2024-03-26 21:35:15 +0500

    organize the folders for controller and model

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require('./controller.php');
+require('./app/controllers/ProductController.php');
 
-$controller = new Controller();
+$controller = new ProductController();
 $controller->index();
 
 // remove php closing tag as per psr if file contain only php remove php closing tag
