9386db03f9c89d901239203e844800c52c20e9e0
razabangi <muhammadrazabangi9@gmail.com>
2024-03-26 21:12:50 +0500

    connect with database using PDO | fetch the data

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "hello world";
+$dns = "mysql:host=localhost;dbname=mvc_php_db;charset=utf8;port=3306";
+$userName = "root";
+$password = "root";
+
+$pdo = new PDO($dns, $userName, $password, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+$stmt = $pdo->query("select * from products");
+
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($products);
