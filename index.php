27ae1e9dff85fcfad21ccd4f0dc05bec9b5806de
razabangi <muhammadrazabangi9@gmail.com>
2024-03-26 21:17:00 +0500

    showing fetched data in html

--- index.php
+++ index.php
@@ -11,5 +11,20 @@ $pdo = new PDO($dns, $userName, $password, [
 $stmt = $pdo->query("select * from products");
 
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
 
-print_r($products);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <?php foreach($products as $product): ?>
+        <h2><?= htmlspecialchars($product['name']); ?></h2>
+        <p><?= htmlspecialchars($product['description']); ?></p>
+    <?php endforeach; ?>
+</body>
+</html>
