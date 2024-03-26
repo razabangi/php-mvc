9eb306114becf5c2805080fd62c8e0ce7280ffb7
razabangi <muhammadrazabangi9@gmail.com>
2024-03-26 22:08:07 +0500

    refactor the front controller logic | index.php for showing response as per request within the index.php

--- index.php
+++ index.php
@@ -4,23 +4,7 @@
 $action = $_GET['action'] ?? null;
 $controller = $_GET['controller'] ?? null;
 
-if ($controller == "product") {
-    require('./app/controllers/ProductController.php');
-    $controllerObj = new ProductController();
-} elseif ($controller == "home") {
-    require('./app/controllers/HomeController.php');
-    $controllerObj = new HomeController();
-} else {
-    require('./app/controllers/HomeController.php');
-    $controllerObj = new HomeController();
-}
-
-if ($action === 'index') {
-    $controllerObj->index();
-} else if ($action == 'show') {
-    $controllerObj->show();
-} else {
-    $controllerObj->index();
-}
-
-// remove php closing tag as per psr if file contain only php remove php closing tag
+$controller = ucfirst($controller) . 'Controller';
+require("./app/controllers/$controller".'.php');
+$controllerObj = new $controller;
+$controllerObj->$action();
