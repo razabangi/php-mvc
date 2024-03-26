2f9d20bffef797811dc1a7b1d706eb38cd22f733
razabangi <muhammadrazabangi9@gmail.com>
2024-03-26 21:20:21 +0500

    make model for get data from model in view

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
 
-$dns = "mysql:host=localhost;dbname=mvc_php_db;charset=utf8;port=3306";
-$userName = "root";
-$password = "root";
+require('./model.php');
 
-$pdo = new PDO($dns, $userName, $password, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-]);
-
-$stmt = $pdo->query("select * from products");
-
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$product = new Model();
+$products = $product->getData();
 ?>
 
 <!DOCTYPE html>
