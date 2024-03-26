794bca2d222e0364d32ef6f5507e65cca68a81ed
razabangi <muhammadrazabangi9@gmail.com>
2024-03-26 22:54:55 +0500

    add pretry urls by using htaccess | add .htaccess file

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
 
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$segments = explode('/', $path);
+
+//request => https://mvc-php.text/product/show
+// Array
+// (
+//     [0] => 
+//     [1] => product
+//     [2] => show
+// )
+
 // front controller
-$action = $_GET['action'] ?? null;
-$controller = $_GET['controller'] ?? null;
+$action = $segments[2] ?? 'index'; // show
+$controller = $segments[1] ?? null; // product
 
 $controller = ucfirst($controller) . 'Controller';
 require("./app/controllers/$controller".'.php');
