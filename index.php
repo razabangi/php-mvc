55d14c4d8429dafb27d2274ab61fd482d4ffd304
razabangi <muhammadrazabangi9@gmail.com>
2024-04-14 14:55:11 +0500

    resolve override issue

--- index.php
+++ index.php
@@ -11,12 +11,12 @@ spl_autoload_register(function(string $className) {
 });
 
 $router = new Router();
-// $router->add("/{controller}/{action}");
-$router->add("/products/{slug:[\w-]+}", ['controller' => 'products', 'action' => 'show']);
+$router->add("/product/{slug:[\w-]+}", ['controller' => 'products', 'action' => 'show']);
 $router->add("/{controller}/{id:\d+}/{action}");
 $router->add('/products/show', ['controller' => "products", 'action' => 'show']);
 $router->add('/', ['controller' => "home", 'action' => 'index']);
 $router->add('/products', ['controller' => "products", 'action' => 'index']);
+$router->add("/{controller}/{action}");
 
 if ($segments = $router->match($path)) {
 } else {
